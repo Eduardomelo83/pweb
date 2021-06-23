70aac44c975923c4077b2eac7c624560dc5f77ac
Eduardo Melo <deveduardomelo@gmail.com>
2021-06-23 16:05:08 -0300

    all

---
+++ Index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Section</title>
+</head>
+<body>
+    <form action="section.php" method="post">
+        <div class="form-control">
+            <label for="name">Digite o seu nome:</label>
+            <input type="text" name="name">
+        </div>
+        <div class="form-control">
+            <label for="phone">Digite o seu telefone:</label>
+            <input type="text" name="phone">
+        </div>
+        <button type="submit">Pronto</button>
+    </form>
+</body>
+</html>
